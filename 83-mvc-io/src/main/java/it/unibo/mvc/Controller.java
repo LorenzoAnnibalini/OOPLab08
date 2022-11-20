079127c554b39ef619aa81e363b9e188ec5eb1de
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2022-11-20 17:22:17 +0100

    Finito Controller

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    void setNextStringtoPrint(final String stringa);
+
+    String getNextStringtoPrint();
+
+    List<String> getPrintedStringsHystory();
+
+    void printCurrentString();
 
 }
