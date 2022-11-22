2d7fb36f692cf95ab689b8980876d7b5d1d40c4a
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2022-11-22 15:32:46 +0100

    Commentato codice non utilizzato

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -2,7 +2,7 @@ package it.unibo.mvc;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
